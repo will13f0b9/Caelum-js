2fcfd31083c68db8592c4cfa7610b30debbafbdf
will13f0b9 <william_rocha123@hotmail.com>
2018-02-06 17:54:57 -0200

    testee dia 2

--- array.js
+++ array.js
@@ -9,3 +9,13 @@ var carros = []; // array vazio
 //array construtor
 
 var cars = new Array(); // array vazio
+console.log(carros.constructor,cars.constructor1);
+console.dir(Array.prototype);
+
+carros.push("gol", "corsa", "palio")    // push usa pra adicionar um elemento no final do array
+carros.unshift("fox");                  // adiciona um elemento no começo do array
+carros.pop();                           // deleta o ultimo elemento do array
+carros.shift();                         // deleta o primeiro elemtno do array
+
+console.log("carros" , carros);
+console.log("cars" , cars); 
